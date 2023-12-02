90f147cbd88bb2db5b30c207c4db8aa22777f264
atharvsharma021 <sharmaatharv2020@gmail.com>
2023-12-02 12:46:05 -0500

    Initial Creation

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Helper/distanceSensor.java
@@ -0,0 +1,10 @@
+package org.firstinspires.ftc.teamcode.Helper;
+import com.qualcomm.robotcore.hardware.DistanceSensor;
+
+import org.firstinspires.ftc.teamcode.twoCamera;
+
+public class distanceSensor {
+    DistanceSensor distanceR = twoCamera.distanceR;
+    DistanceSensor distanceL = twoCamera.distanceL;
+
+}
