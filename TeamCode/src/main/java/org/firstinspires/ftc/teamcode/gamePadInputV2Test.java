2c2c7c6764be9c7439e3d3cd624c0652103613ac
atharvsharma021 <sharmaatharv2020@gmail.com>
2023-12-16 13:03:46 -0500

    gamePadInputV2 test

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/gamePadInputV2Test.java
@@ -0,0 +1,102 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+import org.firstinspires.ftc.teamcode.Helper.DrivetrainV2;
+import org.firstinspires.ftc.teamcode.Helper.gamePadInputV2;
+
+
+import java.util.Locale;
+
+@TeleOp(name= "Atharv is awesome gamePadInputV2 test ", group ="gamePadInputV2")
+public class gamePadInputV2Test extends LinearOpMode {
+    private int tlm_MainLoopCount = 0;
+
+    @Override
+    public void runOpMode() {
+        // Load Introduction and Wait for Start
+        telemetry.setDisplayFormat(Telemetry.DisplayFormat.MONOSPACE);
+        telemetry.addLine("Coach Test - TeleOp Drive Test");
+        telemetry.addLine();
+        telemetry.addData(">", "Press Start to Launch");
+        telemetry.update();
+        DcMotor viperMotor = hardwareMap.dcMotor.get("viperMotor");
+        CRServo conveyor = hardwareMap.crservo.get("conveyor");
+        int power = 1;
+
+        waitForStart();
+        if (isStopRequested()) return;
+        telemetry.clear();
+
+        gamePadInputV2 gpIn = new gamePadInputV2(gamepad1);
+        DrivetrainV2 drvTrain = new DrivetrainV2(hardwareMap);
+        update_telemetry(gpIn, drvTrain);
+
+        while (opModeIsActive()) {
+            ++tlm_MainLoopCount;
+            update_telemetry(gpIn, drvTrain);
+            gamePadInputV2.GameplayInputType inpType = gpIn.WaitForGamepadInput(500);
+            switch (inpType) {
+
+                /*
+                case LEFT_TRIGGER_ON:
+                    viperMotor.setPower(-0.5);
+                    break;
+                case LEFT_TRIGGER_OFF:
+                    viperMotor.setPower(0);
+                    break;
+                case RIGHT_TRIGGER_ON:
+                    viperMotor.setPower(0.5);
+                    break;
+                case RIGHT_TRIGGER_OFF:
+                    viperMotor.setPower(0);
+                    break;
+
+                 */
+                case BUTTON_A:
+                    conveyor.setPower(power);
+                    break;
+                case NONE:
+                    conveyor.setPower(0);
+                    break;
+                case BUTTON_B:
+                    power = -power;
+                case JOYSTICK:
+                    // Set Drivetrain Power
+                    drvTrain.setDriveVectorFromJoystick(  gamepad1.left_stick_x,
+                            gamepad1.right_stick_x, gamepad1.left_stick_y );
+                    break;
+            }
+        }
+    }
+
+
+    private void update_telemetry(gamePadInputV2 gpi, DrivetrainV2 drv) {
+        telemetry.addLine().addData( "Main Loop Cnt", tlm_MainLoopCount);
+
+        telemetry.addLine("Game-pad Input");
+        telemetry.addLine().addData( "Input Ct", gpi.getTelemetry_InputCount());
+        telemetry.addLine().addData( "Inp Last", gpi.getTelemetry_InputLastType().toString());
+        String inpTime = new java.text.SimpleDateFormat("yyyy.MM.dd HH:mm:ss.SSS", Locale.US).format(gpi.getTelemetry_InputLastTimestamp());
+        telemetry.addLine().addData( "Inp Time", inpTime);
+        telemetry.addLine().addData( "L Joy  X", "%6.3f", gamepad1.left_stick_x).addData("Y", "%6.3f", gamepad1.left_stick_y);
+        telemetry.addLine().addData( "R Joy  X", "%6.3f", gamepad1.right_stick_x).addData("Y", "%6.3f", gamepad1.right_stick_y);
+        drv.getTelemetryLastPowerFrontLeft();
+        telemetry.addLine("Drive Train");
+        telemetry.addLine().addData( "Pwr  L F", drv.getTelemetryLastPowerFrontLeft());
+        telemetry.addLine().addData( "Pwr  L B", drv.getTelemetryLastPowerBackLeft());
+        telemetry.addLine().addData( "Pwr  R F", drv.getTelemetryLastPowerFrontRight());
+        telemetry.addLine().addData( "Pwr  R B", drv.getTelemetryLastPowerBackRight());
+        telemetry.addLine().addData( "Braking ", drv.getBrakeStatus());
+        telemetry.addLine().addData( "Brake Ct", drv.getTelemetryBrakeCount());
+        telemetry.addLine().addData( "BTimeout", drv.getTelemetryBrakeTimeoutCount());
+
+        telemetry.update();  // Update clears screen in (Default) Auto Clear Mode
+    }
+
+}
+
