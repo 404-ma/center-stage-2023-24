0e0e68cd3f44be440201aa8988305ed41f939bc6
jaltrichter <59672713+jaltrichter@users.noreply.github.com>
2024-01-02 12:21:07 -0500

    Update DistanceSystem to Sensors

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DistanceSystemtest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DistanceSystemtest.java
@@ -8,36 +8,43 @@ import com.qualcomm.robotcore.util.Range;
 import org.firstinspires.ftc.robotcore.external.Telemetry;
 import org.firstinspires.ftc.teamcode.Helper.DrivetrainV2;
 import org.firstinspires.ftc.teamcode.Helper.DistanceSystem;
+import org.firstinspires.ftc.teamcode.helper.TargetPose;
+
 public class DistanceSystemtest extends LinearOpMode {
     private DrivetrainV2 drv;
+    private DistanceSystem distSys;
 
     @Override
     public void runOpMode() {
         telemetry.setDisplayFormat(Telemetry.DisplayFormat.MONOSPACE);
-        telemetry.addLine("DistanceSystemtest");
+        telemetry.addLine("Distance System Test");
         telemetry.addLine();
         telemetry.addData(">", "Press Start to Launch");
         telemetry.update();
 
         drv = new DrivetrainV2(hardwareMap);
-
+        distSys = new DistanceSystem(hardwareMap);
 
         waitForStart();
         if (isStopRequested()) return;
         telemetry.clear();
+        boolean first = true;
         while (opModeIsActive()) {
-            double distance = DistanceSystem.getDistance();
-            double rangeError = (10 - distance);
+            TargetPose pose = distSys.getTargetPose(first);
+            first = false;
 
+            double rangeError = (10 - pose.range);
 
             // Use the speed and turn "gains" to calculate how we want the robot to move.
-            double forward = Range.clip(rangeError * 0.05, -0.3, 0.3);
+            double forward = Range.clip(rangeError * 0.1, -0.3, 0.3);
+            double rotate = Range.clip(-pose.yaw * 0.01, -0.25, 0.25);
 
 
-            telemetry.addData("Distance", distance);
+            telemetry.addData("Distance", pose.range);
+            telemetry.addData("Yaw", pose.yaw);
             telemetry.update();
 
-            drv.setDriveVector(forward, 0, 0);
+            drv.setDriveVector(forward, 0, rotate);
 
         }
     }
