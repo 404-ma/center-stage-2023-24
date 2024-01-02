912dd2e19f600a90d58989ad59d66169a087c81d
jaltrichter <jaltrichter@outlook.com>
2024-01-02 13:15:52 -0500

    Change in Robot Configuration

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DistanceSystemtest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DistanceSystemtest.java
@@ -1,6 +1,7 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.util.Range;
@@ -10,6 +11,7 @@ import org.firstinspires.ftc.teamcode.Helper.DrivetrainV2;
 import org.firstinspires.ftc.teamcode.Helper.DistanceSystem;
 import org.firstinspires.ftc.teamcode.helper.TargetPose;
 
+@TeleOp(name= "Distance System Test", group ="Test")
 public class DistanceSystemtest extends LinearOpMode {
     private DrivetrainV2 drv;
     private DistanceSystem distSys;
@@ -33,11 +35,12 @@ public class DistanceSystemtest extends LinearOpMode {
             TargetPose pose = distSys.getTargetPose(first);
             first = false;
 
-            double rangeError = (10 - pose.range);
+            double rangeError = (pose.range - 5);
+
 
             // Use the speed and turn "gains" to calculate how we want the robot to move.
-            double forward = Range.clip(rangeError * 0.1, -0.3, 0.3);
-            double rotate = Range.clip(-pose.yaw * 0.01, -0.25, 0.25);
+            double forward = Range.clip(-rangeError * 0.1, -0.3, 0.3);
+            double rotate = Range.clip(pose.yaw * 0.01, -0.25, 0.25);
 
 
             telemetry.addData("Distance", pose.range);
