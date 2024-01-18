cf62873242e0511e2d92dd783ba6f13e6cdd519c
jaltrichter <jaltrichter@outlook.com>
2024-01-18 18:03:00 -0500

    Add

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RRAutoDrive3.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RRAutoDrive3.java
@@ -1,7 +1,10 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.acmerobotics.dashboard.FtcDashboard;
+import com.acmerobotics.dashboard.config.Config;
 import com.acmerobotics.roadrunner.Action;
 import com.acmerobotics.roadrunner.Pose2d;
+import com.acmerobotics.roadrunner.Vector2d;
 import com.acmerobotics.roadrunner.ftc.Actions;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
@@ -9,8 +12,20 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import org.firstinspires.ftc.robotcore.external.Telemetry;
 import org.firstinspires.ftc.teamcode.RoadRunner.MecanumDrive;
 
-@Autonomous (name = "RR Auto Drive 3 - Spike Mark 2  ", group = "RoadRunner")
+@Config
+@Autonomous (name = "RR Auto Drive 3 - Spike Marks", group = "RoadRunner")
 public class RRAutoDrive3 extends LinearOpMode {
+    /*
+     *  FTC Dashboard Parameters
+     */
+    public static class Params {
+        public double propSpikeMark = 1;    //  Which Spike Mark is the Prop Located on
+    }
+
+    public static Params PARAMS = new Params();
+
+    private FtcDashboard dashboard;
+    private MecanumDrive drive;
 
     @Override
     public void runOpMode() {
@@ -21,20 +36,56 @@ public class RRAutoDrive3 extends LinearOpMode {
         telemetry.addData(">", "Press Start to Launch");
         telemetry.update();
 
-        MecanumDrive drive = new MecanumDrive(hardwareMap, new Pose2d(0, 0, 0));
+        dashboard = FtcDashboard.getInstance();
+        dashboard.clearTelemetry();
+        drive = new MecanumDrive(hardwareMap, new Pose2d(0, 0, 0));
 
         waitForStart();
         if (isStopRequested()) return;
         telemetry.clear();
 
-        Action moveOne = drive.actionBuilder(drive.pose)
-                .lineToX(25)
-                .lineToX(7)
-                .turnTo(Math.toRadians(-90))
-                .lineToY(34)
-                .turnTo(Math.toRadians(-100))
-                .build();
-        Actions.runBlocking(moveOne);
+        switch ((int) PARAMS.propSpikeMark) {
+            case 1:
+                // Left Spike Mark
+                Action moveOne = drive.actionBuilder(drive.pose)
+                        .splineTo( new Vector2d(25, -3), Math.toRadians(-45))
+                        .build();
+                Actions.runBlocking(moveOne);
+                break;
+
+            case 3:
+                // Right Spike Mark
+                Action moveTwo = drive.actionBuilder(drive.pose)
+                        .splineTo( new Vector2d(25, 3), Math.toRadians(15))
+                        .setReversed(true)
+                        .splineTo( new Vector2d(7, 0), Math.toRadians(0))
+                        .build();
+                Actions.runBlocking(moveTwo);
+                break;
+
+            default:
+                // Center Spike Mark
+                Action moveThree = drive.actionBuilder(drive.pose)
+                        .lineToX(25)
+                        .lineToX(7)
+                        .turnTo(Math.toRadians(-90))
+                        .build();
+                Actions.runBlocking(moveThree);
+
+        }
+
+
+        if (PARAMS.propSpikeMark == 2) {
+            Action moveBar = drive.actionBuilder(drive.pose)
+                    .lineToX(25)
+                    .lineToX(7)
+                    .turnTo(Math.toRadians(-90))
+                    .lineToY(34)
+                    .turnTo(Math.toRadians(-100))
+                    .build();
+            Actions.runBlocking(moveBar);
+        }
+
 
         sleep(1000);
     }
