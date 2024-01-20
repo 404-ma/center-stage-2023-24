2330307bce9b06d3c96f87252aace651ae607f0d
anuthiru <149186696+anuthiru@users.noreply.github.com>
2024-01-20 18:39:13 -0500

    updated

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RRAutoDrive4.java
@@ -0,0 +1,42 @@
+package org.firstinspires.ftc.teamcode;
+import com.acmerobotics.roadrunner.Action;
+import com.acmerobotics.roadrunner.Pose2d;
+import com.acmerobotics.roadrunner.Vector2d;
+import com.acmerobotics.roadrunner.ftc.Actions;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+import org.firstinspires.ftc.teamcode.RoadRunner.MecanumDrive;
+
+@Autonomous(name = "RR Auto Drive 4 - Box", group = "RoadRunner")
+public class RRAutoDrive4 extends LinearOpMode {
+
+    @Override
+    public void runOpMode() {
+        telemetry.setDisplayFormat(Telemetry.DisplayFormat.MONOSPACE);
+        telemetry.addLine("RoadRunner Auto Drive 2 - Box");
+        telemetry.addLine();
+        telemetry.addData(">", "Press Start to Launch");
+        telemetry.update();
+
+        MecanumDrive drive = new MecanumDrive(hardwareMap, new Pose2d(0, 0, 0));
+
+        waitForStart();
+        if (isStopRequested()) return;
+        telemetry.clear();
+
+        Action moveOne = drive.actionBuilder(drive.pose)
+                .turnTo(Math.toRadians(-90))
+                .waitSeconds(1)
+                .lineToY(48)
+                .waitSeconds(1)
+                .build();
+        Actions.runBlocking(moveOne);
+
+    }
+}
+
+
+
+
+
