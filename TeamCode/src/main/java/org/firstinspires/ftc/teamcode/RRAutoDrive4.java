87375204487df8caf4495aaf0ab8de85ca6e45b7
jaltrichter <jaltrichter@outlook.com>
2024-01-24 18:02:28 -0500

    Changes in Robot Design

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RRAutoDrive4.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RRAutoDrive4.java
@@ -1,5 +1,7 @@
 package org.firstinspires.ftc.teamcode;
+
 import com.acmerobotics.dashboard.FtcDashboard;
+import com.acmerobotics.dashboard.config.Config;
 import com.acmerobotics.roadrunner.Action;
 import com.acmerobotics.roadrunner.Pose2d;
 import com.acmerobotics.roadrunner.Vector2d;
@@ -9,8 +11,17 @@ import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import org.firstinspires.ftc.robotcore.external.Telemetry;
 import org.firstinspires.ftc.teamcode.RoadRunner.MecanumDrive;
 
+@Config
 @Autonomous(name = "RR Auto Drive 4 - Box", group = "RoadRunner")
 public class RRAutoDrive4 extends LinearOpMode {
+    public static class Params {
+        public double endX = 18;
+        public double endY = 60;
+        public double endHeading = -90;
+    }
+
+    public static Params PARAMS = new Params();
+
     private FtcDashboard dashboard;
     private MecanumDrive drive;
 
@@ -24,20 +35,31 @@ public class RRAutoDrive4 extends LinearOpMode {
 
         dashboard = FtcDashboard.getInstance();
         dashboard.clearTelemetry();
-        drive = new MecanumDrive(hardwareMap, new Pose2d(5.83, 33.7, 0));
+        drive = new MecanumDrive(hardwareMap, new Pose2d(0, 0, 0));
 
         waitForStart();
         if (isStopRequested()) return;
         telemetry.clear();
 
+        // Right Spike Mark
+        Action moveTwo = drive.actionBuilder(drive.pose)
+                .splineTo( new Vector2d(18, 3), Math.toRadians(30))
+                .setReversed(true)
+                .splineTo( new Vector2d(6, 0), Math.toRadians(180))
+                .build();
+        Actions.runBlocking(moveTwo);
+
+        Action moveBar = drive.actionBuilder(drive.pose)
+                .turnTo(Math.toRadians(-90))
+                .lineToY(34)
+                .build();
+        Actions.runBlocking(moveBar);
+
         Action moveOne = drive.actionBuilder(drive.pose)
                 .setReversed(true)
-                .lineToY(45)
-                .waitSeconds(2)
-                .splineTo( new Vector2d(23, 60), Math.toRadians(0))
+                .splineTo( new Vector2d(PARAMS.endX,  PARAMS.endY), Math.toRadians(PARAMS.endHeading))
                 .build();
         Actions.runBlocking(moveOne);
-
     }
 
 
