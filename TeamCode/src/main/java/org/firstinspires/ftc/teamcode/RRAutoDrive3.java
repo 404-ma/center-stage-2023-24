f20376f9a3f4215659f52b46eb3aa867c6ecd7d8
anuthiru <149186696+anuthiru@users.noreply.github.com>
2024-01-25 20:40:56 -0500

    updated

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RRAutoDrive3.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RRAutoDrive3.java
@@ -8,6 +8,7 @@ import com.acmerobotics.roadrunner.Vector2d;
 import com.acmerobotics.roadrunner.ftc.Actions;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.Servo;
 
 import org.firstinspires.ftc.robotcore.external.Telemetry;
 import org.firstinspires.ftc.teamcode.RoadRunner.MecanumDrive;
@@ -19,13 +20,24 @@ public class RRAutoDrive3 extends LinearOpMode {
      *  FTC Dashboard Parameters
      */
     public static class Params {
+        public double armForward = 1;
+        public double armUpPos = 0.295;
+        public double armDownPos = 0.2;
+        public double flipForward = 1;
+        public double flipDownPos = 0.54;
+        public double flipSuplexPos = 0.395;
+        public double gripForward = 1;
+        public double gripOpenPos = 0.28;
+        public double gripClosedPos = 0.10;
         public double propSpikeMark = 1;    //  Which Spike Mark is the Prop Located on
     }
+
 
     public static Params PARAMS = new Params();
 
     private FtcDashboard dashboard;
     private MecanumDrive drive;
+
 
     @Override
     public void runOpMode() {
@@ -35,6 +47,11 @@ public class RRAutoDrive3 extends LinearOpMode {
         telemetry.addLine();
         telemetry.addData(">", "Press Start to Launch");
         telemetry.update();
+
+        Servo arm = hardwareMap.servo.get("ArmServo");
+        Servo flip = hardwareMap.servo.get("FlipServo");
+        Servo grip = hardwareMap.servo.get("ClawServo");
+
 
         dashboard = FtcDashboard.getInstance();
         dashboard.clearTelemetry();
@@ -49,30 +66,48 @@ public class RRAutoDrive3 extends LinearOpMode {
                 // Left Spike Mark
                 Action moveOne = drive.actionBuilder(drive.pose)
                         .splineTo( new Vector2d(17, -3), Math.toRadians(-27))
+                        .build();
+                Actions.runBlocking(moveOne);
+
+                PlacePixel(arm, flip, grip);
+
+                Action moveOneb = drive.actionBuilder(drive.pose)
                         .setReversed(true)
                         .splineTo(new Vector2d(6,0),Math.toRadians(180))
                         .build();
-                Actions.runBlocking(moveOne);
+
+                Actions.runBlocking(moveOneb);
                 break;
 
             case 1:
                 // Right Spike Mark
                 Action moveTwo = drive.actionBuilder(drive.pose)
                         .splineTo( new Vector2d(18, 3), Math.toRadians(30))
+                        .build();
+                Actions.runBlocking(moveTwo);
+
+                PlacePixel(arm, flip, grip);
+                Action moveBack = drive.actionBuilder(drive.pose)
                         .setReversed(true)
                         .splineTo( new Vector2d(6, 0), Math.toRadians(180))
                         .build();
-                Actions.runBlocking(moveTwo);
+                Actions.runBlocking(moveBack);
+
                 break;
 
             default:
                 // Center Spike Mark
                 Action moveThree = drive.actionBuilder(drive.pose)
                         .lineToX(21)
+                        .build();
+                Actions.runBlocking(moveThree);
+
+                PlacePixel(arm, flip, grip);
+
+                Action moveThreeb = drive.actionBuilder(drive.pose)
                         .lineToX(6)
                         .build();
-
-                Actions.runBlocking(moveThree);
+                Actions.runBlocking(moveThreeb);
 
 
         }
@@ -118,8 +153,29 @@ public class RRAutoDrive3 extends LinearOpMode {
 
        }
 
+    }
 
 
+    public void PlacePixel(Servo arm, Servo flip, Servo grip){
+        // Reset Claw to Down and Open
+        arm.setPosition(PARAMS.armDownPos);
+        sleep(100);  // let Arm Move Away from Conveyor
+        flip.setPosition(PARAMS.flipDownPos);
+        sleep(500);
+        grip.setPosition(PARAMS.gripOpenPos);
+    }
+
+    public void Retract(Servo arm, Servo flip, Servo grip){
+        arm.setPosition(PARAMS.armUpPos);
+        sleep(100);
+        flip.setPosition(PARAMS.flipSuplexPos);
+        // Wait for Suplex to Finish
+        grip.setPosition(PARAMS.gripOpenPos);
+    }
+    public void Start( Servo arm,Servo flip, Servo grip){
+        grip.setPosition(PARAMS.gripClosedPos);
+        arm.setPosition(PARAMS.armUpPos);
+        flip.setPosition(PARAMS.flipSuplexPos);
 
         sleep(1000);
 
