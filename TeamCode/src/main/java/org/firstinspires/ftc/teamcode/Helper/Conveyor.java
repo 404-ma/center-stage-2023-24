e45d045f85317319f8a16e9d4c0ae478c377e112
jaltrichter <jaltrichter@outlook.com>
2024-01-26 22:05:27 -0500

    Initialization

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Helper/Conveyor.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Helper/Conveyor.java
@@ -6,12 +6,9 @@ import com.acmerobotics.dashboard.config.Config;
 import com.acmerobotics.roadrunner.ftc.Encoder;
 import com.acmerobotics.roadrunner.ftc.OverflowEncoder;
 import com.acmerobotics.roadrunner.ftc.RawEncoder;
-import com.qualcomm.robotcore.hardware.CRServo;
-import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
-import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.HardwareMap;
-import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.hardware.CRServo;
 
 @Config
 public class Conveyor {
@@ -21,27 +18,15 @@ public class Conveyor {
     }
 
     public static Params PARAMS = new Params();
-    public DcMotorEx leftFront, leftBack, rightBack, rightFront;
 
-    private Servo motor;
-
-    public Encoder par0;
+    public DcMotorEx viperMotor;
+    public Encoder viperEncoder;
+    private CRServo conv;
 
     public Conveyor(@NonNull HardwareMap hdwMap){
-        DcMotor viperMotor = hdwMap.dcMotor.get("viperMotor");
-        CRServo conveyor = hdwMap.crservo.get("conveyor");
-
-        leftFront.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        leftBack.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        rightBack.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        rightFront.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-
-        // Reverse motor directions if needed
-        leftFront.setDirection(DcMotorSimple.Direction.FORWARD);
-        leftBack.setDirection(DcMotorSimple.Direction.FORWARD);
-        rightFront.setDirection(DcMotorSimple.Direction.FORWARD);
-
-        par0 = new OverflowEncoder(new RawEncoder(hdwMap.get(DcMotorEx.class, "viperMotor")));
+         viperMotor =   hdwMap.get(DcMotorEx.class, "viperMotor");
+         viperEncoder = new OverflowEncoder(new RawEncoder(hdwMap.get(DcMotorEx.class, "viperMotor")));
+         conv = hdwMap.crservo.get("conveyor");
     }
 
 
