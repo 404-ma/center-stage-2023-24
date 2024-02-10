ffffcf48d892f21a53794830b0b2d1f0416f0617
anuthiru <149186696+anuthiru@users.noreply.github.com>
2024-02-10 09:22:14 -0500

    updates

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Helper/Conveyor.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Helper/Conveyor.java
@@ -6,15 +6,21 @@ import com.acmerobotics.dashboard.config.Config;
 import com.acmerobotics.roadrunner.ftc.Encoder;
 import com.acmerobotics.roadrunner.ftc.OverflowEncoder;
 import com.acmerobotics.roadrunner.ftc.RawEncoder;
+import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.util.Range;
 
 @Config
 public class Conveyor {
 
-    public static class Params{
-        // TODO:  Add Params Values for Conveyor and Viper speeds
+    public static class Params {
+
+        public double conveyorSpeed = 0.7;
+        public double viperSpeed = 0.5;
+        public int viperMotorMaxPositionRelative = 3000;
+
     }
 
     public static Params PARAMS = new Params();
@@ -22,37 +28,44 @@ public class Conveyor {
     public Encoder viperEncoder;
     private CRServo conv;
 
-    public Conveyor(@NonNull HardwareMap hdwMap){
-         viperMotor =   hdwMap.get(DcMotorEx.class, "viperMotor");
-         // TODO: Set ZeroPowerBehavior - Determine what action should be...
-         // TODO: Add Odometry to Viper Motor
-         viperEncoder = new OverflowEncoder(new RawEncoder(hdwMap.get(DcMotorEx.class, "viperMotor")));
-         conv = hdwMap.crservo.get("conveyor");
+
+    private int viperMotorStart;
+
+    public Conveyor(@NonNull HardwareMap hdwMap) {
+        viperMotor = hdwMap.get(DcMotorEx.class, "viperMotor");
+        viperMotor.setZeroPowerBehavior(DcMotorEx.ZeroPowerBehavior.BRAKE);
+        conv = hdwMap.crservo.get("conveyor");
+        viperMotorStart = viperMotor.getCurrentPosition();
     }
 
-    public void moveConvForward(){
+    public void moveConvForward() {
         conv.setPower(0.7);
     }
 
-    public void moveConvBackward(){
+    public void moveConvBackward() {
         conv.setPower(-0.7);
     }
-    public void stopConv(){
+
+    public void stopConv() {
 
         conv.setPower(0);
     }
 
-    public void moveViper(){
-        // TODO:  Encoporate Encoder --viperEncoder.getPositionAndVelocity()
-        // TODO:  Add Min and Max Position Checks to prevent Breaking Viper
-        viperMotor.setPower(0.5);
-    }
-    public void moveDownViper(){
-        viperMotor.setPower(-0.5);
+    public void moveViperToPosition(int position) {
+        int absolutePosition = viperMotorStart + Range.clip(position, 0, PARAMS.viperMotorMaxPositionRelative);
+        viperMotor.setTargetPosition(absolutePosition);
+        viperMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
     }
 
-    public void stopViper(){
-        viperMotor.setPower(0);
+    public void moveViperWithPower(int power){
+        int viperPosition = viperMotor.getCurrentPosition();
+        if ((viperPosition <= viperMotorStart) | (viperPosition >= viperMotorStart +PARAMS.viperMotorMaxPositionRelative))
+            viperMotor.setPower(0);
+        else {
+            viperMotor.setPower( Range.clip(power,-1,1));
+        }
     }
+
+
 
 }
