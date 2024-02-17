2ecd9d38b1a44dc1a70cbe6113f5a54500fed957
jaltrichter <jaltrichter@outlook.com>
2024-02-16 20:56:49 -0500

    Add Crane

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriveControl.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriveControl.java
@@ -9,6 +9,7 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 
 import org.firstinspires.ftc.robotcore.external.Telemetry;
 import org.firstinspires.ftc.teamcode.Helper.ClawMoves;
+import org.firstinspires.ftc.teamcode.Helper.Crane;
 import org.firstinspires.ftc.teamcode.Helper.DeferredActions;
 import org.firstinspires.ftc.teamcode.Helper.DeferredActions.DeferredActionType;
 import org.firstinspires.ftc.teamcode.Helper.DrivetrainV2;
@@ -18,34 +19,30 @@ import java.util.List;
 import java.util.Locale;
 
 
-// TODO: Lets Make a Group for Competition - Maybe Refactor to Different Name
-@TeleOp(name = "DriveFirstMeet", group = "Competition!!")
-public class DriveFirstMeet extends LinearOpMode {
-    // TODO: Add @Config PARAMS values for all the speed multiplier Values
-    // TODO: Add a Version Number Parameter
-
+@TeleOp(name = "Driver Control", group = "Competition!!")
+public class DriveControl extends LinearOpMode {
     private boolean setReversed = false;
     private ClawMoves yclaw;
+    private Crane crane;
 
     @Override
     public void runOpMode() {
         // Load Introduction and Wait for Start
         telemetry.setDisplayFormat(Telemetry.DisplayFormat.MONOSPACE);
-        telemetry.addLine("TeleOp Drive Test");
+        telemetry.addLine("Driver Control");
+        telemetry.addData("Version Number", "3.2");
         telemetry.addLine();
         telemetry.addData(">", "Press Start to Launch");
-        telemetry.update();
-        telemetry.addData("Version Number", "1:3");
         telemetry.update();
 
         // TODO:  Control viperMotor and conveyor servo via helper class.
         DcMotor viperMotor = hardwareMap.dcMotor.get("viperMotor");
         CRServo conveyor = hardwareMap.crservo.get("ConveyorServo");
-        DcMotor craneMotor = hardwareMap.dcMotor.get("craneMotor");
         gamePadInputV2 gpIn1 = new gamePadInputV2(gamepad1);
         gamePadInputV2 gpIn2 = new gamePadInputV2(gamepad2);
         DrivetrainV2 drvTrain = new DrivetrainV2(hardwareMap);
         yclaw = new ClawMoves(hardwareMap);
+        crane = new Crane(hardwareMap);
 
         waitForStart();
         if (isStopRequested()) return;
@@ -136,27 +133,34 @@ public class DriveFirstMeet extends LinearOpMode {
             gamePadInputV2.GameplayInputType inpType2 = gpIn2.WaitForGamepadInput(30);
             switch (inpType2) {
                 // TODO: Is the Minimum Speed Really 0.75 - That make the Trigger range (0.75 - 1)
+                case BUTTON_A:
+                    crane.moveCraneToPosition(0);
+                    break;
 
+                case BUTTON_Y:
+                    crane.moveCraneToPosition(7900);
+                    break;
+
+                case DPAD_DOWN:
+                    crane.moveCraneToRelativePosition(-500);
+                    break;
+
+                case DPAD_UP:
+                    crane.moveCraneToRelativePosition(500);
+                    break;
 
                 case LEFT_TRIGGER:
                     double power = Math.min(gamepad2.left_trigger, 0.75);
                     conveyor.setPower(power);
                     break;
+
                 case RIGHT_TRIGGER:
                     double powerBack = Math.min(gamepad2.right_trigger, 0.75);
                     conveyor.setPower(-powerBack);
                     break;
+
                 case JOYSTICK:
                     viperMotor.setPower(gamepad2.right_stick_y * -0.5);
-                    //craneMotor.setPower(gamepad2.left_stick_y * -0.8);
-
-                    float strafe = 0;
-                    if ((gamepad1.left_stick_x >= 0.5f) && (gamepad1.left_stick_x <= -0.5f))
-                        strafe = gamepad1.left_stick_x;
-
-                    float forward = ((gamepad1.left_stick_y >= 0.5f) ? gamepad1.left_stick_x : 0f ) * 0.25f;
-
-                            drvTrain.setDriveVectorFromJoystick(0, 0, 0, false);
                     break;
             }
             // Deferred Actions
