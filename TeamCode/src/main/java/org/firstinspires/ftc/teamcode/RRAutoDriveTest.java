8f48b8f876954dce89c00a65ae8ab0c782df25a6
jaltrichter <jaltrichter@outlook.com>
2024-03-02 16:23:43 -0500

    Testing

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RRAutoDriveTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RRAutoDriveTest.java
@@ -1,25 +1,30 @@
 package org.firstinspires.ftc.teamcode;
 
+import androidx.annotation.NonNull;
+
 import com.acmerobotics.roadrunner.Action;
+import com.acmerobotics.roadrunner.Arclength;
 import com.acmerobotics.roadrunner.Pose2d;
-import com.acmerobotics.roadrunner.Vector2d;
+import com.acmerobotics.roadrunner.Pose2dDual;
+import com.acmerobotics.roadrunner.PosePath;
+import com.acmerobotics.roadrunner.TranslationalVelConstraint;
+import com.acmerobotics.roadrunner.TurnConstraints;
+import com.acmerobotics.roadrunner.VelConstraint;
 import com.acmerobotics.roadrunner.ftc.Actions;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
-import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
 import org.firstinspires.ftc.robotcore.external.Telemetry;
 import org.firstinspires.ftc.teamcode.RoadRunner.MecanumDrive;
 
-@Disabled
-@Autonomous (name = "RR Auto Drive 2 - Box", group = "RoadRunner")
-public class RRAutoDrive2 extends LinearOpMode {
+@Autonomous (name = "RR Auto Drive Test", group = "RoadRunner")
+public class RRAutoDriveTest extends LinearOpMode {
 
     @Override
     public void runOpMode() {
         // Load Introduction and Wait for Start
         telemetry.setDisplayFormat(Telemetry.DisplayFormat.MONOSPACE);
-        telemetry.addLine("RoadRunner Auto Drive 2 - Box");
+        telemetry.addLine("RoadRunner Auto Drive Test");
         telemetry.addLine();
         telemetry.addData(">", "Press Start to Launch");
         telemetry.update();
@@ -31,19 +36,21 @@ public class RRAutoDrive2 extends LinearOpMode {
         telemetry.clear();
 
         Action moveOne = drive.actionBuilder(drive.pose)
-                .lineToX(40)
-                .waitSeconds(1)
+                .lineToX(20, new TranslationalVelConstraint(20))
                 .turnTo(Math.toRadians(90))
                 .waitSeconds(1)
                 .lineToY(30)
                 .waitSeconds(1)
-                .turnTo(Math.toRadians(180))
+                .turnTo(Math.toRadians(180), new TurnConstraints(15, 5, 25))
                 .build();
         Actions.runBlocking(moveOne);
 
-        sleep(1000);
 
-        Action moveTwo = drive.actionBuilder(drive.pose)
+
+
+        sleep(1000);
+        /*
+             Action moveTwo = drive.actionBuilder(drive.pose)
                 .lineToX(0)
                 .waitSeconds(1)
                 .turnTo(Math.toRadians(270))
@@ -54,6 +61,8 @@ public class RRAutoDrive2 extends LinearOpMode {
                 .waitSeconds(1)
                 .build();
         Actions.runBlocking(moveTwo);
+         */
+
     }
 
 }
